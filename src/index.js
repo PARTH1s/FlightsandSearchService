54b16e30f593dbc4bf5f2aacedc28b89c96d7ed9
Parth <parthsahani4786@gmail.com>
2025-07-18 23:28:55 +0530

    Completed basic sequelize setup to crete a DB

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express=require('express')
 
 const bodyParser=require('body-parser')
 
-const {PORT}=require('./conifg/serverConfig')
+const {PORT}=require('./config/serverConfig')
 
 const SetUpandStartServer=async()=>{
     // create the express object 
