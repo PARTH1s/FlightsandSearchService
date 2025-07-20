9f9e8ac0a6b81b7c797afb14a6f341d0af0e5bb9
Parth <parthsahani4786@gmail.com>
2025-07-20 12:06:11 +0530

    Added routes to complete create city api

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ const bodyParser=require('body-parser')
 
 const {PORT}=require('./config/serverConfig'); 
  
+const ApiRoutes = require('./routes/index');
+
 const SetUpandStartServer=async()=>{
     // create the express object 
     const app = express(); 
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
+
+    app.use('/api',ApiRoutes);
 
     app.listen(PORT,async ()=>{
         console.log(`Server Started at ${PORT}`);  
